c964c4312ca8594576b744482c5148d03e5c8618
HectorRC920 <hector.rubio.920@gmail.com>
2022-12-05 22:45:29 -0600

    aliases

---
+++ src/typeScriptCourse/16.alias.ts
@@ -0,0 +1,18 @@
+(( )=> {
+  type  UserID = string | number | boolean
+  type TSizes =  "SM" | "ME" | "LA" | "2XL";
+  function greeting(userId: UserID, sizes: TSizes) {
+    if(typeof userId == 'number'){
+      console.log(`User id es numbero`);
+    }else if(typeof userId == 'boolean'){
+      console.log(`User id es bool`);
+    } else {
+      console.log(`User id es string`);
+    }
+  }
+  greeting('ola','2XL')
+  greeting(2,'LA')
+  greeting(true,2)
+
+  //LiteralTypes
+})()
